20f3622be80a070b5dfc75b02c90c15875e403b2
jamesvongampai <jaythomasv29@gmail.com>
2022-11-15 10:16:35 -0800

    add mvn spring project with initialr

--- server/src/main/java/com/healthportal/demo/DemoApplication.java
+++ server/src/main/java/com/healthportal/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.kaiser.demo;
+package com.healthportal.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- server/src/test/java/com/healthportal/demo/DemoApplicationTests.java
+++ server/src/test/java/com/healthportal/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kaiser.demo;
+package com.healthportal.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
