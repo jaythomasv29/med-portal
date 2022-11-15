cade70a8c6fd86aa0100fe0113df4d70336f13b4
Valzhina <valzhinapiskun@gmail.com>
2022-11-15 15:27:24 -0500

    Enitial commit Olya

--- server/src/main/java/com/healthportal/models/Appointment.java
+++ server/src/main/java/com/healthportal/models/Appointment.java
@@ -1,9 +1,46 @@
 package com.healthportal.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Appointments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Appointment {
 
-    private String Department;
-    private Patient patient;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JsonBackReference
     private Doctor doctor;
 
+    @ManyToOne
+    @JsonBackReference
+    private Patient patient;
+
+    @Column
+    private String Department;
+
+    @Column
+    private java.sql.Date rDate;
+
+    @Column
+    private java.sql.Time rTime;
+
+    @Column
+    //Could be new patient or following up appointment
+    private String appcategory;
+
+    @Column
+    //confirmed or not confirmed
+    private String status;
 }
