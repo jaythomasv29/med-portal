26823b97929262f198f21709be0f4e3b42814985
oseicanusee <89167475+oseicanusee@users.noreply.github.com>
2022-11-15 14:45:38 -0500

    Doctor Entity Updated

---
+++ server/src/main/java/com/healthportal/dtos/DoctorDTO.java
@@ -0,0 +1,34 @@
+package com.healthportal.dtos;
+
+import com.healthportal.models.Doctor;
+import com.healthportal.models.Patient;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DoctorDTO {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String doctor_id;
+    private List<Patient> patientList;
+
+    public DoctorDTO(Doctor doctor){
+        this.id = doctor.getId();
+        this.firstName = doctor.getFirstName();
+        this.lastName = doctor.getLastName();
+        this.doctor_id = doctor.getDoctor_id();
+        this.patientList = doctor.getPatientList();
+    }
+
+    
+
+
+
+
+}
--- server/src/main/java/com/healthportal/models/Doctor.java
+++ server/src/main/java/com/healthportal/models/Doctor.java
@@ -1,10 +1,16 @@
 package com.healthportal.models;
 
-import javax.print.Doc;
+import com.healthportal.dtos.DoctorDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import java.util.List;
-import java.util.Objects;
 
 
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Doctor {
     // fields go here
     private long id;
@@ -23,67 +29,12 @@ public class Doctor {
         this.patientList = patientList;
     }
 
-    public long getId() {
-        return id;
+    public Doctor(DoctorDTO doctorDTO){
+        this.firstName = doctorDTO.getFirstName();
+        this.lastName = doctorDTO.getLastName();
+        this.doctor_id = doctorDTO.getDoctor_id();
+        this.patientList = doctorDTO.getPatientList();
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getDoctor_id() {
-        return doctor_id;
-    }
-
-    public void setDoctor_id(String doctor_id) {
-        this.doctor_id = doctor_id;
-    }
-
-    public List<Patient> getPatientList() {
-        return patientList;
-    }
-
-    public void setPatientList(List<Patient> patientList) {
-        this.patientList = patientList;
-    }
-
-    @Override
-    public String toString() {
-        return "Doctor{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", doctor_id='" + doctor_id + '\'' +
-                ", patientList=" + patientList +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Doctor)) return false;
-        Doctor doctor = (Doctor) o;
-        return id == doctor.id && firstName.equals(doctor.firstName) && lastName.equals(doctor.lastName) && doctor_id.equals(doctor.doctor_id) && patientList.equals(doctor.patientList);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, firstName, lastName, doctor_id, patientList);
-    }
 }
