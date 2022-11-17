30a6e9bbcad28a40bc662966e04b3e942942473a
Valzhina <valzhinapiskun@gmail.com>
2022-11-17 16:03:02 -0500

    AppointmentController

---
+++ server/src/main/java/com/healthportal/controllers/AppointmentController.java
@@ -0,0 +1,50 @@
+package com.healthportal.controllers;
+
+import com.healthportal.dtos.AppointmentDTO;
+import com.healthportal.dtos.PatientDTO;
+import com.healthportal.services.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/appointments")
+public class AppointmentController {
+    @Autowired
+    private AppointmentService appointmentService;
+
+    @GetMapping("/patient/{patientId}")
+    public List<PatientDTO> getAppointmentByPatient(@PathVariable Long patientId){
+        return appointmentService.getAllAppointmentsByPatientId(patientId);
+    }
+
+    @GetMapping("/doctor/{doctorId}")
+    public List<AppointmentDTO> getAppointmentByDoctor(@PathVariable Long doctorId){
+        return appointmentService.getAllAppointmentsByDoctorId(doctorId);
+    }
+
+//    @GetMapping("/{appointmentId}")
+//    public Optional<AppointmentDTO> getAppointmentById(@PathVariable Long TBDId){
+//        return appointmenService.getRequestById(TBDId);
+//    }
+
+    @PostMapping("/patient/{patientId}")
+    public void addAppointment(@RequestBody AppointmentDTO appointmentDto,@PathVariable Long patientId) {
+        appointmentService.addAppointment(appointmentDto, patientId);
+    }
+
+    @DeleteMapping("/{appointmentId}")
+    public void deleteAppointmentById(@PathVariable Long requestId){ appointmentService.deleteAppointmentById(appointmentId);
+    }
+
+    @PutMapping("/{appointmentId}")
+    public void changeStatusAppointmentById(@PathVariable Long appointmentId){
+        appointmentService.changeStatusAppointmentById(appointmentId);
+    }
+
+    @PutMapping
+    public void updateAppointment(@RequestBody AppointmentDTO requestDto){ appointmentService.updateAppointmentById(appointmentDto);
+    }
+}
